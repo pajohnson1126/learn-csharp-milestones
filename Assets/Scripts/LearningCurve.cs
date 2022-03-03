f5618f8682933ec05395d3f723db35c91b10c525
Paige Johnson <pajohnson@davidson.edu>
2022-03-03 12:05:46 -0500

    ch 2 script in unity

---
+++ Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    /* Document for C# Milestones
+     * Author: Paige Johnson
+     * DIG 250
+     */
+
+
+    // defining variables
+    int age = 30;
+    float weight = 150.3;
+    string name = "Paige";
+    bool state = true;
+
+    // Start is called before the first frame update
+    /// <summary>
+	/// runs at before start
+	/// </summary>
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    /// <summary>
+	/// updates game scene
+	/// </summary>
+    void Update()
+    {
+        
+    }
+}
