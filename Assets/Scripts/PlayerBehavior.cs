36e14290082b61fab238e9cd74e8cab5b261cb49
Paige Johnson <pajohnson@davidson.edu>
2022-03-22 14:49:03 -0400

    chapter 8

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    public GameObject Bullet;
+    public float BulletSpeed = 100f;
+    private bool _isShooting;
+
+    public float DistanceToGround = 0.1f;
+    public LayerMask GroundLayer;
+    public CapsuleCollider _col;
+
     public float MoveSpeed = 10f;
     public float RotateSpeed = 75f;
 
     private float _vInput;
     private float _hInput;
 
+    public float JumpVelocity = 5f;
+    private bool _isJumping;
+
     private Rigidbody _rb;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+
+        _col = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        _isShooting |= Input.GetMouseButtonDown(0);
+        _isJumping |= Input.GetKeyDown(KeyCode.Space);
+
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
 
@@ -31,6 +47,13 @@ public class PlayerBehavior : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (IsGrounded() && _isJumping)
+        {
+            _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+        }
+
+        _isJumping = false;
+
         Vector3 rotation = Vector3.up * _hInput;
 
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
@@ -38,5 +61,23 @@ public class PlayerBehavior : MonoBehaviour
         _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
 
         _rb.MoveRotation(_rb.rotation * angleRot);
+
+        if (_isShooting)
+        {
+            GameObject newBullet = Instantiate(Bullet, this.transform.position + new Vector3(1, 0, 0), this.transform.rotation);
+            Rigidbody BulletRB = newBullet.GetComponent<Rigidbody>();
+            BulletRB.velocity = this.transform.forward * BulletSpeed;
+        }
+
+        _isShooting = false;
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, DistanceToGround, GroundLayer, QueryTriggerInteraction.Ignore);
+
+        return grounded;
     }
 }
